085948479a653cd3f230fda80016f4e01a14a2cc
unknown <Administrator@claudia-pc.temasoft.pub>
2015-10-06 08:34:50 +0300

    pb from part 2

---
+++ InterestRate.Tests/InterestRateTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InterestRate;
+
+namespace InterestRate.Tests
+{
+    [TestClass]
+    public class InterestRateTests
+    {
+        [TestMethod]
+        public void InterestRateTest1()
+        {
+            int loan= 40000;
+            double rate= 7.57;
+            int duration = 20;
+            int rateOfMonth = 39; //luna martie a celui de-al 4-lea an
+
+            double rateToBePaid = InterestRate.CalculateRate(loan, rate, duration, rateOfMonth);
+            Assert.AreEqual(377.49,Math.Round(rateToBePaid,2));
+        }
+    }
+}
---
+++ InterestRate/InterestRate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InterestRate
+{
+    [TestClass]
+    public class InterestRate
+    {
+        [TestMethod]
+        public static double CalculateRate(int loan, double rate, int duration, int rateOfMonth)
+        {
+            float capital = 0;
+            double monthlyRate = 0;
+            double rata = 0;
+            capital = loan / (duration*12);
+            monthlyRate = rate / 12;
+            rata = capital + (monthlyRate) / 100 * (loan - capital * rateOfMonth);
+            return rata;   
+        }
+    }
+}
